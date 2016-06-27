5a7797ff7a097419ca79513127007c1a6043e7fa
Chris Myers <cmyers78@gmail.com>
2016-06-27 16:24:57 -0600

    currently have one answer displaying in a tag....need to generate randomly

--- QandA/QandA/AnswerViewController.swift
+++ QandA/QandA/AnswerViewController.swift
@@ -12,17 +12,31 @@ class AnswerViewController: UIViewController {
 
     var passedQuestion : Question?
     
+    var random = arc4random_uniform(4) + 1
+    
+    let answerArray = ["Bluth", "John Stossel", "Hotdogs", "Bobby Dodd Stadium", "Denver", "Joseph", "Dooley", "Medium-Well"]
+    
+    @IBOutlet weak var answer1Outlet: UIButton!
+    
+    
     @IBOutlet weak var questionLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    
 
         if let pQ = self.passedQuestion {
             self.questionLabel.text = pQ.questionString
+            self.answer1Outlet.setTitle(pQ.answerString, forState: .Normal)
+            
         }
+        
+        
     
     
     }
 
+    @IBAction func Answer1Pressed(sender: UIButton) {
+    }
     
 }
